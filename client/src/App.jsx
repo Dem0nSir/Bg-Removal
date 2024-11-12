6f8bf2a09a1a2b7a9b580d91d7731aa239f87e4d
Dem0nSir <sthanishan21@gmail.com>
2024-11-12 21:15:56 +0545

    Updates the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
+import React from 'react'
+import {Routes, Route} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Result from "./pages/Result.jsx";
+import BuyCredit from "./pages/BuyCredit.jsx";
+
 function App() {
+
     return (
         <>
-App
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/result" element={<Result/>}/>
+                <Route path="/buy" element={<BuyCredit/>}/>
+            </Routes>
         </>
     )
 }
