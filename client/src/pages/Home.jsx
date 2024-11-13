c424eca2424c8fbc0168f1e52d1b583d4c2d3df5
Dem0nSir <sthanishan21@gmail.com>
2024-11-13 20:47:52 +0545

    update on design on front page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from "../components/Header.jsx";
 import Steps from "../components/Steps.jsx";
+import BgSlider from "../components/BgSlider.jsx";
+import Testimonials from "../components/Testimonials.jsx";
 
 const Home = () => {
     return (
         <div>
             <Header/>
             <Steps/>
+            <BgSlider/>
+            <Testimonials/>
         </div>
     )
 }
