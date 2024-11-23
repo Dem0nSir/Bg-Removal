aac93ec327264e46186ffbda802b9d63f67556c6
Dem0nSir <sthanishan21@gmail.com>
2024-11-23 18:46:45 +0545

    updates fixes

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -4,7 +4,7 @@ const connectDB = async ()=>{
     mongoose.connection.on('connected', () => {
         console.log('Connected to MongoDB')
     })
-    await mongoose.connect(`${process.env.MONGODB_URI}/background-remover`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`)
 }
 
 export default connectDB
